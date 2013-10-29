a84b7b68af91a0cfbc42fd08a5b4992dae89b8c1
Alex Githatu <2128805+kalengi@users.noreply.github.com>
2013-10-30 02:17:56 +0300

    -- removed original menu action -- added wizard -- added menu action to start wizard

---
+++ src/co/ke/nerdonia/ndzf2modules/ZF2ModuleCreatorWizardPanel1.java
@@ -0,0 +1,71 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.ke.nerdonia.ndzf2modules;
+
+import javax.swing.event.ChangeListener;
+import org.openide.WizardDescriptor;
+import org.openide.util.HelpCtx;
+
+public class ZF2ModuleCreatorWizardPanel1 implements WizardDescriptor.Panel<WizardDescriptor> {
+
+    /**
+     * The visual component that displays this panel. If you need to access the
+     * component from this class, just use getComponent().
+     */
+    private ZF2ModuleCreatorVisualPanel1 component;
+
+    // Get the visual component for the panel. In this template, the component
+    // is kept separate. This can be more efficient: if the wizard is created
+    // but never displayed, or not all panels are displayed, it is better to
+    // create only those which really need to be visible.
+    @Override
+    public ZF2ModuleCreatorVisualPanel1 getComponent() {
+        if (component == null) {
+            component = new ZF2ModuleCreatorVisualPanel1();
+        }
+        return component;
+    }
+
+    @Override
+    public HelpCtx getHelp() {
+        // Show no Help button for this panel:
+        return HelpCtx.DEFAULT_HELP;
+        // If you have context help:
+        // return new HelpCtx("help.key.here");
+    }
+
+    @Override
+    public boolean isValid() {
+        // If it is always OK to press Next or Finish, then:
+        return true;
+        // If it depends on some condition (form filled out...) and
+        // this condition changes (last form field filled in...) then
+        // use ChangeSupport to implement add/removeChangeListener below.
+        // WizardDescriptor.ERROR/WARNING/INFORMATION_MESSAGE will also be useful.
+    }
+
+    @Override
+    public void addChangeListener(ChangeListener l) {
+    }
+
+    @Override
+    public void removeChangeListener(ChangeListener l) {
+    }
+
+    @Override
+    public void readSettings(WizardDescriptor wiz) {
+        // use wiz.getProperty to retrieve previous panel state
+    }
+
+    @Override
+    public void storeSettings(WizardDescriptor wiz) {
+        // use wiz.putProperty to remember current panel state
+        wiz.putProperty("module name", getModuleNameFromVisualPanel());
+    }
+    
+    private String getModuleNameFromVisualPanel() {
+        return ((ZF2ModuleCreatorVisualPanel1) component).getModuleName();
+   }
+}
