b4b3a877145738c3ee5d4f4395e6d025eb418392
Alex Githatu <2128805+kalengi@users.noreply.github.com>
2013-10-30 00:33:37 +0300

    -- initial commit -- "New ZF2 Module" menu showing

---
+++ build/classes-generated/co/ke/nerdonia/ndzf2modules/Bundle.java
@@ -0,0 +1,13 @@
+package co.ke.nerdonia.ndzf2modules;
+/** Localizable strings for {@link co.ke.nerdonia.ndzf2modules}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>New ZF2 Module</i>
+     * @see ZF2ModuleActionListener
+     */
+    static String CTL_ZF2ModuleActionListener() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_ZF2ModuleActionListener");
+    }
+    private void Bundle() {}
+}
